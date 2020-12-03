6d4ca881087ae55c83492bc20dc549e090079ea3
ajcarvajal <alanjcarv@gmail.com>
2020-12-02 16:37:04 -0800

    clean up and add rustfmt

--- aoc2020/day1/src/main.rs
+++ aoc2020/day1/src/main.rs
@@ -1,34 +1,32 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::collections::HashMap;
-
-
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-        let file = File::open(filename)?;
-        Ok(io::BufReader::new(file).lines())
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
-
 
 fn get_input() -> Vec<i32> {
     let mut result = vec![];
 
     for line in read_lines("input.txt").expect("file error") {
-        result.push(match line {
-            Ok(l) => l.parse::<i32>().unwrap_or(0),
-            Err(_) => {0},
-        });
+        match line {
+            Ok(l) => result.push(l.parse::<i32>().unwrap_or(0)),
+            Err(_) => (),
+        };
     }
     result
 }
 
-
 fn part1(input: &Vec<i32>) -> i32 {
     let mut diffs = HashMap::<i32, i32>::new();
 
-    for num in input { 
+    for num in input {
         match diffs.get(&(2020 - num)) {
             Some(n) => return (2020 - n) * num,
             None => diffs.insert(*num, 2020 - num),
@@ -37,12 +35,11 @@ fn part1(input: &Vec<i32>) -> i32 {
     -1
 }
 
-
 fn part2(input: &Vec<i32>) -> i32 {
     // TODO: Fix brute force implementation
     for i in 0..input.len() {
-        for j in i+1..input.len() {
-            for k in j+1..input.len() {
+        for j in i + 1..input.len() {
+            for k in j + 1..input.len() {
                 if input[i] + input[j] + input[k] == 2020 {
                     return input[i] * input[j] * input[k];
                 }
@@ -51,7 +48,6 @@ fn part2(input: &Vec<i32>) -> i32 {
     }
     -1
 }
-
 
 fn main() {
     let input = get_input();
--- aoc2020/day2/src/main.rs
+++ aoc2020/day2/src/main.rs
@@ -1,8 +1,7 @@
+use std::convert::TryFrom;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::convert::TryFrom;
-
 
 #[derive(Debug)]
 struct PasswordPolicy {
@@ -12,75 +11,67 @@ struct PasswordPolicy {
     password: String,
 }
 
-
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-        let file = File::open(filename)?;
-        Ok(io::BufReader::new(file).lines())
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
 
-
 fn get_input() -> Vec<PasswordPolicy> {
-    let mut raw_result = vec![];
+    let mut result = vec![];
 
     for line in read_lines("input.txt").expect("file error") {
-        raw_result.push(match line {
-            Ok(l) => {
-                l.split(|c| c == '-' || c == ' ' || c == ':')
-                 .map(String::from)
-                 .collect::<Vec<String>>()
-            },
-            Err(_) => {vec![]},
-        });
-    }
+        let raw_result = line
+            .unwrap()
+            .split(|c| c == '-' || c == ' ' || c == ':')
+            .map(String::from)
+            .collect::<Vec<String>>();
 
-    // Parse fields into a struct
-    let mut result = Vec::<PasswordPolicy>::new();
-    for line in raw_result {
-        result.push( 
-            PasswordPolicy { min: line[0].parse::<u32>().unwrap(),
-                             max: line[1].parse::<u32>().unwrap(),
-                             letter: line[2].parse::<char>().unwrap(),
-                             password: line[4].clone() }
-            );
-        
+        result.push(PasswordPolicy {
+            min: raw_result[0].parse::<u32>().unwrap(),
+            max: raw_result[1].parse::<u32>().unwrap(),
+            letter: raw_result[2].parse::<char>().unwrap(),
+            password: raw_result[4].to_owned(),
+        });
     }
     result
 }
-
 
 fn part1(input: &Vec<PasswordPolicy>) -> u32 {
     let mut result = 0;
 
-    for password in input {
+    for line in input {
         let mut count = 0;
 
-        for letter in password.password.chars() {
-            if letter == password.letter {
+        for letter in line.password.chars() {
+            if letter == line.letter {
                 count += 1;
             }
         }
-        if count >= password.min && count <= password.max { result += 1; }
+        if count >= line.min && count <= line.max {
+            result += 1;
+        }
     }
     result
 }
-
 
 fn part2(input: &Vec<PasswordPolicy>) -> u32 {
     let mut result = 0;
 
-    for password in input {
-        let idx1 = usize::try_from(password.min - 1).unwrap_or(0);
-        let idx2 = usize::try_from(password.max - 1).unwrap_or(0);
+    for line in input {
+        let idx1 = usize::try_from(line.min - 1).unwrap_or(0);
+        let idx2 = usize::try_from(line.max - 1).unwrap_or(0);
 
-        if (password.password.chars().nth(idx1).unwrap() == password.letter) ^ 
-            (password.password.chars().nth(idx2).unwrap() == password.letter) {
-                result += 1;
+        if (line.password.chars().nth(idx1).unwrap() == line.letter)
+            ^ (line.password.chars().nth(idx2).unwrap() == line.letter)
+        {
+            result += 1;
         }
     }
     result
 }
-
 
 fn main() {
     let input = get_input();
